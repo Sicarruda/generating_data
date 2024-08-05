7bba0fdb16cb5f3ba9cdad9bb83c1747e465a63d
Jessica Arruda <sicarruda@gmail.com>
2024-08-05 19:21:17 -0300

    Altera código para aceitar qualquer quantidade de dados com qualquer quantidade de lados

--- die_visual.py
+++ die_visual.py
@@ -2,37 +2,74 @@ import plotly.express as px
 
 from die import Die
 
+class Die_Visual():
+    # TODO Classe para retornar o resultado não importa quandos dados sejam usados 
 
-rolls = 50000
+    def __init__(self, rolls = 1000, dices=[]):
+        self.rolls = rolls
+        self.dices = dices
+        self.created_dices = {}
+        self.results = []
 
-# TODO Classe para retornar o resultado não importa quandos dados sejam usados 
+        self.create_dices()
+        self.meke_rolls()
 
-# Create two D6 dice.
-die_1 = Die()
-die_2 = Die()
+    def create_dices(self):
+        # Create Dices.
 
-# Make some rolls, and store results in a list.
-results = []
+        for index in range(len(self.dices)):
+            key_name = f"dice_{index}"
+            self.created_dices[key_name] = Die(self.dices[index])
 
-for roll_num in range(rolls):
-    result = die_1.roll() + die_2.roll()
+    def meke_rolls(self):
+        # Make some rolls, and store results in a list.
 
-    results.append(result)
+        result = 0
+        for roll_num in range(self.rolls):
+            for die in self.created_dices.values():
+                result += die.roll()
+            self.results.append(result)
+            result = 0
 
-# Analyze the results.
-frequencies = []
-max_result = die_1.num_sides + die_2.num_sides
-poss_results = range(2, max_result + 1)
-for value in poss_results:
-    frequency = results.count(value)
-    frequencies.append(frequency)
+    def die_max_result(self):
+        # The max result of all dices.
 
-# Visualize the results.
-title = "Results of Rolling Two D6 1.000 TImes"
-labels = {"x": "Result", "y": "Frequency of Results"}
-fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+        max_result = 0
 
-# Further customize chart.
-fig.update_layout(xaxis_dtick=1)
+        for die in self.created_dices.values():
+            max_result += die.num_sides
 
-fig.show()
+        return max_result
+
+    def analyze_result(self):
+        # Analyze the results.
+
+        frequencies = []
+        max_result = self.die_max_result()
+        poss_results = range(len(self.created_dices), max_result + 1)
+
+        for value in poss_results:
+            frequency = self.results.count(value)
+            frequencies.append(frequency)
+
+        return poss_results, frequencies
+    
+    def visualize_grafic(self):
+        # Visualize the results.
+
+        analyzed_result = self.analyze_result()
+        print(analyzed_result)
+
+        title = f"Results of Rolling  1.000 TImes"
+        labels = {"x": "Result", "y": "Frequency of Results"}
+        fig = px.bar(x=analyzed_result[0], y=analyzed_result[1], title=title, labels=labels)
+
+        # Further customize chart.
+        fig.update_layout(xaxis_dtick=1)
+
+        fig.show()
+
+
+die_teste = Die_Visual(5000,dices=[6,6,6])
+
+die_teste.visualize_grafic()
